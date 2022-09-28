1e88986f14a8d795694b5ec9678ac3bba46cdeaf
evgenjnr <evgeniyr7@gmail.com>
2022-09-28 17:13:31 +0300

    auto close nav panel, update styles for timetable view

--- src/app/modules/public/public-page.component.ts
+++ src/app/modules/public/public-page.component.ts
@@ -1,16 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { MatDrawer } from '@angular/material/sidenav';
+import { Event, NavigationEnd, Router } from '@angular/router';
+import { filter, Subject, takeUntil, tap } from 'rxjs';
 
 @Component({
   selector: 'app-public-page',
   templateUrl: './public-page.component.html',
   styleUrls: ['./public-page.component.scss']
 })
-export class PublicPageComponent implements OnInit {
+export class PublicPageComponent implements OnInit, OnDestroy {
+
+  @ViewChild('sideNav', { read: MatDrawer }) sideNavPanel!: MatDrawer;
 
   public sidenavOpenedChange = false;
-  constructor() { }
+
+  private onDestroy$: Subject<void> = new Subject();
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+    this.router.events.pipe(
+      filter((ev) => ev instanceof NavigationEnd),
+      tap(() => {
+        this.sideNavPanel.close();
+      }),
+      takeUntil(this.onDestroy$)
+    ).subscribe();
+  }
+
+  ngOnDestroy(): void {
+    this.onDestroy$.next();
+    this.onDestroy$.complete();
   }
 
 }
